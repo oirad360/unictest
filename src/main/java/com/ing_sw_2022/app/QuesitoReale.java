93c0cdd616798c44bcd9ba91abe2a2f4d20ba50b
Dario Anzalone <oirad36043@gmail.com>
2022-01-23 17:41:28 +0100

    classi nuove

---
+++ src/main/java/com/ing_sw_2022/app/QuesitoReale.java
@@ -0,0 +1,19 @@
+package progettazione;
+
+public class QuesitoReale {
+
+	private String id;
+
+	private Risposta[] risposteDate {Map};
+
+	private QuesitoDescrizione quesitoDescrizione;
+
+	public void selezionaRisposta(String idRisposta) {
+
+	}
+
+	public int verificaCorrettezza() {
+		return 0;
+	}
+
+}
---
+++ src/main/java/com/ing_sw_2022/app/Test.java
@@ -0,0 +1,21 @@
+package progettazione;
+
+public class Test {
+
+	private String id;
+
+	private float punteggioComplessivo;
+
+	private TemplatePersonalizzato templatePersonalizzato;
+
+	private QuesitoReale[] mappaQuesiti {Map};
+
+	public void selezionaRisposta(String idQuesitoReale, String idRisposta) {
+
+	}
+
+	public Test terminaSimulazione() {
+		return null;
+	}
+
+}
