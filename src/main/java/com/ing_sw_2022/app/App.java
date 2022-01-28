f959b370553a701ba688d0ea44a8e941945edf6b
Dario Anzalone <oirad36043@gmail.com>
2022-01-28 14:19:13 +0100

    itextpdf, generazione pdf (vedi main)

--- src/main/java/com/ing_sw_2022/app/App.java
+++ src/main/java/com/ing_sw_2022/app/App.java
@@ -1,10 +1,17 @@
 package com.ing_sw_2022.app;
 
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import com.itextpdf.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Hello world!
@@ -14,7 +21,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Tesseract tesseract = new Tesseract();
+        /*Tesseract tesseract = new Tesseract();
         tesseract.setDatapath("C:/Users/oirad/unictest/tesseract/tessdata");
         tesseract.setLanguage("ita");
         String text = null;
@@ -23,6 +30,20 @@ public class App
         } catch (TesseractException e) {
             e.printStackTrace();
         }
-        System.out.println(text);
+        System.out.println(text);*/
+        Document document=new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("provaPdf.pdf"));
+            document.open();
+
+            Paragraph paragraph = new Paragraph("AAAAAa aaaaaaaa aaaaaa aaa a aaa");
+            document.add(paragraph);
+
+            document.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
