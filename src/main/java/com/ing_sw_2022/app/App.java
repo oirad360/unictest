4726fb4713fa692ab5935eeb9acd880ee92d0403
Dario Anzalone <oirad36043@gmail.com>
2022-01-28 17:57:30 +0100

    update immagini tesseract

--- src/main/java/com/ing_sw_2022/app/App.java
+++ src/main/java/com/ing_sw_2022/app/App.java
@@ -1,18 +1,12 @@
 package com.ing_sw_2022.app;
 
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.*;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -22,17 +16,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        /*Tesseract tesseract = new Tesseract();
+        Tesseract tesseract = new Tesseract();
         tesseract.setDatapath("C:/Users/oirad/unictest/tesseract/tessdata");
         tesseract.setLanguage("ita");
         String text = null;
         try {
-            text= tesseract.doOCR(new File("C:/Users/oirad/unictest/tesseract/Immagine.png"));
+
+            text= tesseract.doOCR(new File("C:/Users/oirad/unictest/tesseract/immagine6.png"));
+
         } catch (TesseractException e) {
             e.printStackTrace();
         }
-        System.out.println(text);*/
-        Document document=new Document();
+        System.out.println(text);
+        Object[] o=text.lines().toArray();
+        String[] s=new String[o.length];
+        int i=0;
+        for(Object o1:o){
+            s[i]=(String) o1;
+            System.out.println(s[i]);
+            System.out.println("---------");
+        }
+        /*Document document=new Document();
         try {
             PdfWriter.getInstance(document, new FileOutputStream("provaPdf.pdf"));
             document.open();
@@ -40,11 +44,13 @@ public class App
             Paragraph paragraph = new Paragraph("AAAAAa aaaaaaaa aaaaaa aaa a aaa");
             document.add(paragraph);
 
+
             document.close();
         } catch (DocumentException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        }*/
+
     }
 }
